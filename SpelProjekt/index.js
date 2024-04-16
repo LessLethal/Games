e8479bf732811272f8d4145f2f92141967bdd446
LessLethal <alvintrianon@gmail.com>
2024-04-16 09:27:49 +0200

    nothing

--- SpelProjekt/index.js
+++ SpelProjekt/index.js
@@ -1,14 +1,25 @@
+
 let rutor = ["", "", "", "", "", "", "", "", ""];
 
 let Player = "X";
 
+const rutaCell = document.getElementById("ruta");
 
-function endGame(){
+rutaCell.foreach(cell => addEventListener('click', handleclick));
 
+const winningcombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [1, 4, 7],
+  [0, 4, 8],
+  [2, 4, 6],
+  [2, 5, 8],
+];
+
+
+function endgame(){
+location.reload
 }
 
-function win(){
-
-    let rutor = ["", "", "", "", "", "", "", "", ""];
-}
 
