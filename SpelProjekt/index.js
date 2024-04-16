0aecea6aca68bd182d686cce3eecf5e776fa70e9
LessLethal <alvintrianon@gmail.com>
2024-04-16 14:19:26 +0200

    Fixed altering Symbol

--- SpelProjekt/index.js
+++ SpelProjekt/index.js
@@ -1,11 +1,29 @@
-
 let rutor = ["", "", "", "", "", "", "", "", ""];
 
-let Player = "X";
+let currentTurn = 1;
+let player1Turn = 1;
+let player2Turn = 2;
 
-const rutaCell = document.getElementById("ruta");
+let player1 = "O";
+let player2 = "X";
 
-rutaCell.foreach(cell => addEventListener('click', handleclick));
+const cells = document.querySelectorAll(".cell");
+
+cells.forEach((cell) => {
+  cell.addEventListener("click", function () {
+    switch (currTurn) {
+      case player1Turn:
+        cell.textContent = player1;
+        currentTurn++;
+        break;
+
+      case player2Turn:
+        cell.textContent = player2;
+        currentTurn = 1;
+        break;
+    }
+  });
+});
 
 const winningcombos = [
   [0, 1, 2],
@@ -16,10 +34,3 @@ const winningcombos = [
   [2, 4, 6],
   [2, 5, 8],
 ];
-
-
-function endgame(){
-location.reload
-}
-
-
